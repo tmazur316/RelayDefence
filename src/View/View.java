ea1b53fe163d27e6786dab3bebf8776147451c94
Tomasz Mazur <T.Mazur@stud.elka.pw.edu.pl>
2019-05-22 23:06:36 +0200

    Commit 1

--- src/View/View.java
+++ src/View/View.java
@@ -1,6 +1,33 @@
 package View;
 
-public class View
+import javax.swing.*;
+import java.awt.*;
+
+public class View extends JFrame
 {
+
+  public View(){
+    setWindow();
+  }
+
+  private void setWindow(){
+    EventQueue.invokeLater(() -> {
+      JFrame frame = new JFrame("okno");
+      JButton cannon = new JButton("fire");
+      frame.setSize(400, 400);
+      frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+      frame.setLocationRelativeTo(null);
+      frame.setResizable(true);
+      frame.add(cannon);
+      cannon.addActionListener((actionEvent)->{
+        
+      });
+      frame.setVisible(true);
+    });
+  }
+
+  public static void main(String [] args){
+    View view = new View();
+  }
 
 }
